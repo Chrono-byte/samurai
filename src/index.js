0d73082f79b273f1264b2a6ed21a7641ae149692
Michael Gummere <me@disilla.org>
2022-09-30 12:48:15 -0400

    update test

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ const blade = new Katana("./database", { encrypt: true, saveToDisk: true });
 const nameArray = ["John", "Jane", "Jack", "Jill", "Jenny", "Jen", "Bob", "Jackie", "Doc", "Marty", "Amelia", "Daniel", "Terry", "Brandon", "Leo"];
 
 blade.purgeState();
+
 for (let index = 0; index < nameArray.length; index++) {
 	let x = Math.floor(Math.random() * 100000);
-	blade.write(`Hello! ${nameArray[index % nameArray.length]}`, x);
+	blade.write(`Hello! ${nameArray[Math.floor(x / 10000)]}`, x);
 	console.log([x, blade.get(x), blade.has(x)]);
 	blade.overwrite(`Hello! ${nameArray[index % nameArray.length]}`, x);
 }
@@ -15,4 +16,5 @@ for (let index = 0; index < nameArray.length; index++) {
 for (const key in blade.library) {
 	blade.delete(key);
 }
-// console.log(blade.exportState());
+
+console.log(blade.exportState());
