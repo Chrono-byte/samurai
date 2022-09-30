a71a4cd9d03c31e21d7db451f02aeac9a91155e2
Michael Gummere <me@disilla.org>
2022-09-30 11:47:25 -0400

    update samurai

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const { Katana } = require("@chronomly/katana");
-const blade = new Katana("./database", { encrypt: true, saveToDisk: true });
+// const { Katana } = require("@chronomly/katana");
+
+const { Katana } = require("../../");
+const blade = new Katana("./database", { encrypt: false, saveToDisk: false });
+const nameArray = ["John", "Jane", "Jack", "Jill", "Jenny", "Jen", "Bob", "Jackie", "Doc", "Marty", "Amelia", "Daniel"];
 
 blade.purgeState();
-for (let index = 0; index < 1; index++) {
-	blade.push("Hello!", `${Math.random() * 10}`);
+for (let index = 0; index < nameArray.length; index++) {
+	let x = Math.floor(Math.random() * 100000);
+	blade.write(`Hello! ${nameArray[index % nameArray.length]}`, x);
+	let y = blade.get(x);
+	console.log([x, y]);
 }
+
+for (const key in blade.library) {
+	blade.delete(key);
+}
+console.log(blade.exportState());
