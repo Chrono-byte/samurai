3780e7dd7428fc6ee8136471816d255c70d3c965
Michael Gummere <me@disilla.org>
2022-09-30 12:43:21 -0400

    update test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 // const { Katana } = require("@chronomly/katana");
 
 const { Katana } = require("../../");
-const blade = new Katana("./database", { encrypt: false, saveToDisk: false });
-const nameArray = ["John", "Jane", "Jack", "Jill", "Jenny", "Jen", "Bob", "Jackie", "Doc", "Marty", "Amelia", "Daniel"];
+const blade = new Katana("./database", { encrypt: true, saveToDisk: true });
+const nameArray = ["John", "Jane", "Jack", "Jill", "Jenny", "Jen", "Bob", "Jackie", "Doc", "Marty", "Amelia", "Daniel", "Terry", "Brandon", "Leo"];
 
 blade.purgeState();
 for (let index = 0; index < nameArray.length; index++) {
 	let x = Math.floor(Math.random() * 100000);
 	blade.write(`Hello! ${nameArray[index % nameArray.length]}`, x);
-	let y = blade.get(x);
-	console.log([x, y]);
-	console.log(blade.has(x));
+	console.log([x, blade.get(x), blade.has(x)]);
+	blade.overwrite(`Hello! ${nameArray[index % nameArray.length]}`, x);
 }
 
 for (const key in blade.library) {
 	blade.delete(key);
 }
-console.log(blade.exportState());
+// console.log(blade.exportState());
